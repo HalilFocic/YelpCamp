e875fe9ad868bdb0ba28b4ce52967ebb4a6e526c
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-02-04 09:24:03 +0100

    Partials,bootstrap added

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
-
 const Campground = require("./models/campground");
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -15,6 +14,8 @@ db.on("error", console.error.bind(console, "connection error"));
 db.once("open", () => {
   console.log("Database connected!");
 });
+const app = express();
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
