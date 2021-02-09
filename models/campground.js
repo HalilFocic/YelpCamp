18f3e61241ce00826e276e2a6924907911d05f96
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-02-09 16:01:12 +0100

    Review model

--- models/campground.js
+++ models/campground.js
@@ -1,12 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
-    title: String,
-    image: String,
-    price: Number,
-    description: String,
-    location: String
+  title: String,
+  image: String,
+  price: Number,
+  description: String,
+  location: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
 
-module.exports = mongoose.model('Campground', CampgroundSchema);
+module.exports = mongoose.model("Campground", CampgroundSchema);
