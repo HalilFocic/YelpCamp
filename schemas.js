820a9dcceecde7674440c53971a65497310aaaee
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-02-09 16:25:41 +0100

    Rating validation

--- schemas.js
+++ schemas.js
@@ -12,7 +12,7 @@ module.exports.campgroundSchema = campgroundSchema;
 
 module.exports.reviewSchema = Joi.object({
   review: Joi.object({
-    rating: Joi.number().required(),
+    rating: Joi.number().required().min(1).max(5),
     body: Joi.string().required(),
   }).required(),
 });
