654a8219df6e9e95f6090c3cd7c1546bc90f9ee0
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-02-13 17:02:37 +0100

    fake user testing

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 const campgrounds = require("./routes/campgrouds");
 const reviews = require("./routes/reviews");
@@ -30,7 +33,6 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
-
 const sessionConfig = {
   secret: "thisshouldbeabettersecret",
   resave: false,
@@ -44,10 +46,26 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
+});
+
+app.get("/fakeUser", async (req, res) => {
+  const user = new User({
+    email: "testmail@gmail.com",
+    username: "testuser",
+  });
+  const newUser = await User.register(user, "chicken");
+  res.send(newUser);
 });
 
 app.use("/campgrounds", campgrounds);
