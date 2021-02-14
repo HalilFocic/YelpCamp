bea22dd287778ac71ef8cd7f97923376d22bef82
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-02-14 12:07:58 +0100

    Reviews validation & authorization

--- routes/reviews.js
+++ routes/reviews.js
@@ -7,34 +7,27 @@ const ExpressError = require("../utils/ExpressError");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
 const { reviewSchema } = require("../schemas.js");
-
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
-
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 router.post(
   "/",
+  isLoggedIn,
   validateReview,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
     req.flash("success", "Created new review!");
-
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
 
 router.delete(
   "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
   catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
