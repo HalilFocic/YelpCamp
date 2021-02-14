a2e10f45bd96eeb4dbb306423c20042e9aea9cfc
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-02-14 15:35:10 +0100

    controllers for users;

--- routes/reviews.js
+++ routes/reviews.js
@@ -8,34 +8,17 @@ const Campground = require("../models/campground");
 const Review = require("../models/review");
 const { reviewSchema } = require("../schemas.js");
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
-router.post(
-  "/",
-  isLoggedIn,
-  validateReview,
-  catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash("success", "Created new review!");
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
+const reviews = require("../controllers/reviews");
 
+//Route for making a new review
+router.post("/", isLoggedIn, validateReview, catchAsync(reviews.createReview));
+
+//Route for deleting a specific review
 router.delete(
   "/:reviewId",
   isLoggedIn,
   isReviewAuthor,
-  catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Successfully deleted review");
-
-    res.redirect(`/campgrounds/${id}`);
-  })
+  catchAsync(reviews.deleteReview)
 );
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,46 +3,27 @@ const router = express.Router();
 const User = require("../models/user");
 const catchAsync = require("../utils/catchAsync");
 const passport = require("passport");
-router.get("/register", (req, res) => {
-  res.render("users/register");
-});
-router.post(
-  "/register",
-  catchAsync(async (req, res, next) => {
-    try {
-      const { email, username, password } = req.body;
-      const user = new User({ email, username });
-      const registeredUser = await User.register(user, password);
-      req.login(registeredUser, (err) => {
-        if (err) return next();
-        req.flash("success", "Welcome to Yelp Camp!");
-        res.redirect("campgrounds");
-      });
-    } catch (e) {
-      req.flash("error", e.message);
-      res.redirect("/register");
-    }
-  })
-);
-router.get("/login", (req, res) => {
-  res.render("users/login");
-});
+const users = require("../controllers/users");
+
+//Route to render registration form
+router.get("/register", users.renderRegister);
+
+// Route for sending request to make new user
+router.post("/register", catchAsync(users.register));
+
+//Route to render login form
+router.get("/login", users.renderLogin);
+
+//Route to login someone in,or atleast attempt to
 router.post(
   "/login",
   passport.authenticate("local", {
     failureFlash: true,
     failureRedirect: "/login",
   }),
-  (req, res) => {
-    req.flash("success", "Welcome back!");
-    const redirectUrl = req.session.returnTo || "/campgrounds";
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-  }
+  users.login
 );
-router.get("/logout", (req, res) => {
-  req.logout();
-  req.flash("success", "Goodbye!");
-  res.redirect("/campgrounds");
-});
+
+//Route for logging out
+router.get("/logout", users.logout);
 module.exports = router;
