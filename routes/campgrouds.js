650dd3183f617bf3eae109ec0375521915a91fff
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-02-16 12:22:29 +0100

    routes combined for camogrounds

--- routes/campgrouds.js
+++ routes/campgrouds.js
@@ -8,46 +8,30 @@ const flash = require("connect-flash");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 const campgrounds = require("../controllers/campgrounds");
 
-// Main page for campgrounds
-router.get("/", catchAsync(campgrounds.index));
-
-//Route to render form for making new campground
+router
+  .route("/")
+  .get(catchAsync(campgrounds.index))
+  .post(
+    isLoggedIn,
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
-
-//Route to post a request to make new campground
-router.post(
-  "/",
-  isLoggedIn,
-  validateCampground,
-  catchAsync(campgrounds.createCampground)
-);
-
-//Route to render a specific campground
-router.get("/:id", catchAsync(campgrounds.showCampground));
-
-//Route to render editing form for campground
+router
+  .route("/:id")
+  .get(catchAsync(campgrounds.showCampground))
+  .put(
+    isLoggedIn,
+    isAuthor,
+    validateCampground,
+    catchAsync(campgrounds.updateCampground)
+  )
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 router.get(
   "/:id/edit",
   isLoggedIn,
   isAuthor,
   catchAsync(campgrounds.renderEditForm)
-);
-
-//route to send request to edit campground
-router.put(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  validateCampground,
-  catchAsync(campgrounds.updateCampground)
-);
-
-// Route to delete specific campground
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  catchAsync(campgrounds.deleteCampground)
 );
 
 module.exports = router;
