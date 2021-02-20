80b074e7b1de3e32681e9a9341f46323291b9714
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-02-20 09:23:47 +0100

    source chaanged

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -7,9 +7,9 @@ var map = new mapboxgl.Map({
 });
 
 map.on("load", function () {
-  map.addSource("earthquakes", {
+  map.addSource("campgrounds", {
     type: "geojson",
-    data: "https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson",
+    data: campgrounds,
     cluster: true,
     clusterMaxZoom: 14, // Max zoom to cluster points on
     clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
@@ -18,7 +18,7 @@ map.on("load", function () {
   map.addLayer({
     id: "clusters",
     type: "circle",
-    source: "earthquakes",
+    source: "campgrounds",
     filter: ["has", "point_count"],
     paint: {
       "circle-color": [
@@ -37,7 +37,7 @@ map.on("load", function () {
   map.addLayer({
     id: "cluster-count",
     type: "symbol",
-    source: "earthquakes",
+    source: "campgrounds",
     filter: ["has", "point_count"],
     layout: {
       "text-field": "{point_count_abbreviated}",
@@ -49,7 +49,7 @@ map.on("load", function () {
   map.addLayer({
     id: "unclustered-point",
     type: "circle",
-    source: "earthquakes",
+    source: "campgrounds",
     filter: ["!", ["has", "point_count"]],
     paint: {
       "circle-color": "#11b4da",
@@ -66,7 +66,7 @@ map.on("load", function () {
     });
     var clusterId = features[0].properties.cluster_id;
     map
-      .getSource("earthquakes")
+      .getSource("campgrounds")
       .getClusterExpansionZoom(clusterId, function (err, zoom) {
         if (err) return;
 
@@ -77,10 +77,6 @@ map.on("load", function () {
       });
   });
 
-  // When a click event occurs on a feature in
-  // the unclustered-point layer, open a popup at
-  // the location of the feature, with
-  // description HTML from its properties.
   map.on("click", "unclustered-point", function (e) {
     var coordinates = e.features[0].geometry.coordinates.slice();
     var mag = e.features[0].properties.mag;
@@ -92,9 +88,6 @@ map.on("load", function () {
       tsunami = "no";
     }
 
-    // Ensure that if the map is zoomed out such that
-    // multiple copies of the feature are visible, the
-    // popup appears over the copy being pointed to.
     while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
