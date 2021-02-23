0acdf0f23a69783798643d80acb868d44442c482
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-02-23 17:46:54 +0100

    /

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-const port = process.env.PORt || 3000;
+const port = process.env.PORT || 3000;
 app.listen(3000, () => {
   console.log(`Serving on port ${port}`);
 });
