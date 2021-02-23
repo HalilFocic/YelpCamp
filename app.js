7c3f18a288a9db5a49b24e9b1655f2e18d20b9b1
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-02-23 17:21:12 +0100

    eror fixes

--- app.js
+++ app.js
@@ -106,6 +106,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
+const port = process.env.PORt || 3000;
 app.listen(3000, () => {
-  console.log("Serving on port 3000");
+  console.log(`Serving on port ${port}`);
 });
